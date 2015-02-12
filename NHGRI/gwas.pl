87ec1acf917a5e8dbdc1c7d7ac7c4eec8b27f895
ahanden <adam.handen@gmail.com>
2015-02-12 13:18:53 -0500

    Finished the GWAS update with filters. Also added index on gene_synonyms to increase speed.

---
+++ NHGRI/gwas.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use DBI;
+use Term::ReadKey;
+use IO::Handle;
+
+my ($synonym_query, $discontinued_query, $basic_query, %gene_cache);
+STDOUT->autoflush(1);
+
+sub main {
+    my $usage = "Usage: perl gene_info.pl [gene_info]";
+    
+    if(scalar(@ARGV) != 1){
+        print "$usage\n";
+        exit;
+    }
+    
+    ReadMode 1;
+    print "dbname: ";
+    my $db = <STDIN>;
+    chomp $db;
+    ReadMode 1;
+    print "username: ";
+    my $user = <STDIN>;
+    chomp $user;
+    ReadMode 2;
+    print "password: ";
+    my $password = <STDIN>;
+    print "\n";
+    chomp $password;
+    ReadMode 1;
+
+    my $fname = $ARGV[0] || die $usage;
+    my $dbh = DBI->connect("dbi:mysql:$db:localhost",$user,$password,
+        {RaiseError => 1, AutoCommit => 0}) or die;
+
+
+    $basic_query = $dbh->prepare("SELECT entrez_id FROM genes WHERE symbol = ?");
+    $synonym_query = $dbh->prepare("SELECT entrez_id FROM gene_synonyms WHERE symbol = ?");
+    $discontinued_query = $dbh->prepare("SELECT entrez_id FROM discontinued_genes WHERE discontinued_symbol = ?");
+
+    exec_main($dbh, $fname);
+
+    if ($@) {
+        print "Error encountered: rolling back changes.\n";
+        $dbh->rollback();
+        exit 1;
+    }
+    else {
+        $dbh->commit();
+        print "Done inserting genes\n";
+        exit 0;
+    }
+}
+
+sub exec_main {
+    my ($dbh, $fname) = @_;
+    my @data;
+    my %symbols;
+    open(IN,"<$fname") or die "Failed to open $fname: $!\n";
+    while(<IN>){
+        chomp;
+        my @fields = split("\t",$_);
+        my $pubmed = $fields[1];
+        my $trait = $fields[7];
+        my $gene = $fields[13];
+        if($gene && $gene !~ m/intergenic/i && $gene ne 'NR' && $trait) {
+            my @genes = split(/,\s*/,$gene);
+            foreach $gene(@genes) {
+                chomp $gene;
+                $symbols{$gene} = 1;
+                push(@data,[$pubmed, $trait, $gene]);
+            }
+        }
+    }
+    close IN;
+
+    foreach my $symbol(keys(%symbols)) {
+        get_eid($dbh,$symbol);
+    }
+    print "\n";
+
+    my $sth = $dbh->prepare("INSERT IGNORE INTO gwas (pubmed_id,trait,entrez_id) VALUES (?, ?, ?)");
+
+    foreach my $entry(@data) {
+        my $eid = $gene_cache{$entry->[2]};
+        if($eid) {
+            $sth->execute($entry->[0],$entry->[1],$eid);
+        }
+    }
+    print "\n";
+
+    print "Filling database. This may take several minutes...\n";
+}
+
+sub get_eid {
+    my($dbh,$symbol) = @_;
+    $basic_query->execute($symbol);
+    my $eid = $basic_query->fetch();
+    if(!$eid) {
+        $synonym_query->execute($symbol);
+        $eid = $synonym_query->fetch();
+        if(!$eid) {
+            $discontinued_query->execute($symbol);
+            $eid = $discontinued_query->fetch();
+        }
+    }
+    $gene_cache{$symbol} = $eid->[0];
+}
+
+main();
--- db-structure.sql
+++ db-structure.sql
@@ -3,7 +3,8 @@ CREATE TABLE `discontinued_genes` (
   `entrez_id` int(10) unsigned NOT NULL,
   `discontinued_id` int(10) unsigned NOT NULL,
   `discontinued_symbol` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`discontinued_id`)
+  PRIMARY KEY (`discontinued_id`),
+  KEY `discontinued_symbol` (`discontinued_symbol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `experiment_xrefs`;
@@ -34,7 +35,8 @@ DROP TABLE IF EXISTS `gene_synonyms`;
 CREATE TABLE `gene_synonyms` (
   `entrez_id` int(10) unsigned NOT NULL,
   `symbol` varchar(30) NOT NULL,
-  PRIMARY KEY (`entrez_id`,`symbol`)
+  PRIMARY KEY (`entrez_id`,`symbol`),
+  KEY `symbol (symbol)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `gene_xrefs`;
@@ -51,7 +53,8 @@ CREATE TABLE `genes` (
   `symbol` varchar(30) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `tax_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`entrez_id`)
+  PRIMARY KEY (`entrez_id`),
+  KEY `symbol` (`symbol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `go_annotations`;
@@ -68,6 +71,14 @@ CREATE TABLE `go_terms` (
   `category` enum('Component','Function','Process') NOT NULL,
   PRIMARY KEY (`go_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `gwas`;
+CREATE TABLE `gwas` (
+	`pubmed_id` bigint(20) unsigned NOT NULL,
+	`trait` varchar(255) NOT NULL,
+	`entrez_id` varchar(20) NOT NULL,
+	PRIMARY KEY (`pubmed_id`,`trait`,`entrez_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `interaction_xrefs`;
 CREATE TABLE `interaction_xrefs` (
@@ -105,4 +116,10 @@ CREATE TABLE `psi_tree` (
   `psi_id` varchar(7) NOT NULL,
   `is_a` varchar(7) NOT NULL,
   PRIMARY KEY (`psi_id`,`is_a`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `taxonomies`;
+CREATE TABLE `taxonomies` (
+  `tax_id` int(10) unsigned PRIMARY KEY,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
